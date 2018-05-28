1a60d40ed1d079a0fa27d5499faf55a1eee904b6
snirzango <snirzango@gmail.com>
2018-05-28 20:18:27 +0300

    test

---
+++ Duck hunt/Assets/gun/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public int health;
+
+    void Update()
+    {
+        Debug.Log(health.ToString());
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }    
+    }
+}
---
+++ Duck hunt/Assets/gun/GunScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+    public Transform barrel;
+    public float range = 0f;
+
+     void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine("Fire");
+        } 
+    }
+    IEnumerator Fire()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(barrel.position, transform.forward);
+
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            if(hit.collider.tag == "Enemy")
+            {
+                Enemy enemy = hit.collider.GetComponent<Enemy>();
+                enemy.health -= 1;
+            }
+        }
+        Debug.DrawRay(barrel.position, transform.up * range, Color.blue);
+        yield return null;
+    }
+}

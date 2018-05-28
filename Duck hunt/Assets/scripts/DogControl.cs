49c802a26a44692e0fd3e94882daf3e41503fa59
snirzango <snirzango@gmail.com>
2018-05-28 20:23:36 +0300

    a

---
+++ Duck hunt/Assets/scripts/DogControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class DogControl : MonoBehaviour{
+    private float speed;
+    public Vector3 target;
+    private Vector3 start_pos = new Vector3(0, 0, 0);
+    bool ducks_flyed = false;
+    void Start()
+    {
+        speed = 10;
+    }
+
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+        if (target.Equals(transform.position) && !ducks_flyed && transform.position != start_pos) 
+        {
+            ducks_flyed = true;
+            fly_ducks(target);//target means the postion where the ducks started to fly
+        }
+     
+
+    }
+
+    private void fly_ducks(Vector3 target)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Duck hunt/Assets/scripts/DuckCreate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckCreate : MonoBehaviour {
+    private Vector3 start_pos = new Vector3(5, 5, 5);
+    public GameObject duck;
+    private float speed = 10f;
+    private void Start()
+    {
+        
+        GameObject dog = GameObject.Find("Dog");
+        duck = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        duck.transform.position = dog.GetComponent<DogControl>().target;
+        duck.GetComponent<Collider>().isTrigger = false;
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(duck);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(duck);
+    }
+}
